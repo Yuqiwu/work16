e2a19549ce8652bd6c981e7239552a50dff1fc84
Yuqiwu <ywu@stuy.edu>
2017-12-10 21:01:38 -0500

    finished

--- intro.js
+++ intro.js
@@ -1,10 +1,34 @@
-var addItem = function(e){
-    var ol = document.getElementById('thelist');
-    var li = document.createElement("li");
-    var children = ol.children.length
-    li.appendChild( document.createTextNode("item " + children) );
-    ol.appendChild(li);
-}
-
 var b = document.getElementById('b');
+var thelist = document.getElementById('thelist');
+var h = document.getElementById('h');
+var list = document.getElementsByTagName('li');
 
+var allInOne = function(e){
+    var li = document.createElement("li");
+    var len = list.length
+    li.innerHTML = 'item ' + len;
+    li.addEventListener( 'mouseover', changeH);
+    li.addEventListener( 'mouseout', resetH);
+    li.addEventListener( 'click', delList);
+    thelist.appendChild(li);
+};
+
+var changeH = function(e){
+    h.innerHTML = e.target.innerHTML;
+};
+
+var resetH = function(e){
+    h.innerHTML = 'Hello World!';
+};
+
+var delList = function(e){
+    thelist.removeChild(this);
+};
+
+for (var i = 0; i < list.length; i++) {
+  list[i].addEventListener('mouseover', changeH);
+  list[i].addEventListener('mouseout', resetH);
+  list[i].addEventListener('click', delList);
+};
+
+b.addEventListener('click', allInOne);
